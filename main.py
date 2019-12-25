7106062603e4e3e733404fb9d83555265c6951b4
DavidJiang7 <874677397@qq.com>
2019-12-25 15:29:59 +0800

    0.0

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from spider.zmz_list_spider import zmz_list_spider
-from spider.zmz_link_search_spider import zmz_link_search_spider
-from utils.http import UrlTool
+import requests
+from datetime import datetime
 
-# 增量爬取资源
-s = zmz_list_spider()
-s.go()
-input()
-
-# # 爬取下载链接
-# s = Search()
-# s.go()
-# print('爬取下载链接结束......')
-
-# s = '【美剧】《越狱权力的游戏》'
-# ss = s[s.index('《')+1:s.index('》')]
-# print(ss)
+url = 'http://www.google.com'
+print("开始......")
+print(datetime.now())
+response = requests.get(url, timeout=10)
+response.encoding ='utf-8'
+html = response.text
+print(html)
