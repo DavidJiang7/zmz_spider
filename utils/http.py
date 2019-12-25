c083d9677293bde27a5422277989dc105c226dd5
DavidJiang7 <874677397@qq.com>
2019-12-25 08:59:07 +0800

    0.0

--- utils/http.py
+++ utils/http.py
@@ -1,5 +1,5 @@
 from urllib import parse
-import re, requests, random, json
+import re, requests, random, json, traceback
 
 USER_AGENT_LIST = [
     "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1",
